498c03687cd2b54ad142f6633d4b71b1d74aa2b4
Cameron Dougal <cdougal@mit.edu>
2024-03-07 15:36:31 -0500

    Lab 4 6.1-6.5, fix nav current page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script>
+    import { page } from '$app/stores';
+
+    let pages = [
+        {url: "/", title: "Home"},
+        {url: "/projects", title: "Projects"},
+        {url: "/contact", title: "Contact"},
+        {url: "/resume", title: "Resume"},
+        {url: "https://github.com/cammyt5", title: "Github"},
+    ];
+</script>
+
+<style>
+    nav {
+        display: flex;
+        margin-bottom: 3em;
+        border-bottom-width: 0.1em;
+        border-bottom-style: solid;
+        --border-color: oklch(50% 10% 200 / 40%);
+        border-bottom-color: var(--border-color);
+    }
+
+    a {
+        flex: 1;
+        text-decoration: none;
+        color: inherit;
+        text-align: center;
+        padding: 0.5em;
+    }
+
+    a.current {
+        border-bottom-width: 0.3em;
+        border-bottom-style: solid;
+        border-bottom-color: var(--border-color);
+        padding-bottom: 0.2em;
+    }
+
+    a:hover {
+        border-bottom-width: 0.3em;
+        border-bottom-style: solid;
+        padding-bottom: 0.2em;
+        border-bottom-color: color-mix(in oklch, var(--color-accent), canvas 85%);
+    }
+</style>
+
+<nav>
+    {#each pages as p }
+        <a href={ p.url } 
+           class:current={$page.route.id === p.url}
+           target={ p.url.startsWith("http") ? "_blank" : null }>
+                { p.title }
+        </a>
+    {/each}
+</nav>
+
+<slot />
--- static/global.js
+++ static/global.js
@@ -1,39 +1,6 @@
-function $$ (selector, context = document) {
-    return Array.from(context.querySelectorAll(selector));
-}
-
-// let navLinks = $$("nav a")
-// let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
-// currentLink?.classList.add("current");
-
-//create nav on each page
-let pages = [
-	{url: ".", title: "Home"},
-	{url: "projects", title: "Projects"},
-	{url: "contact", title: "Contact"},
-    {url: "resume", title: "Resume"},
-    {url: "https://github.com/cammyt5", title: "Github"},
-];
-
-let nav = document.createElement("nav");
-document.body.prepend(nav);
-const ARE_WE_HOME = document.documentElement.classList.contains("home");
-
-for (let p of pages) {
-	let url = p.url;
-    //url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
-	let title = p.title;
-    let a = document.createElement("a");
-    a.href = url;
-    a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add("current");
-    }
-    if (a.host !== location.host) {
-        a.setAttribute("target", "_blank")
-    }
-    nav.append(a);
-}
+// function $$ (selector, context = document) {
+//     return Array.from(context.querySelectorAll(selector));
+// }
 
 //create theme selector
 document.body.insertAdjacentHTML("afterbegin", `
